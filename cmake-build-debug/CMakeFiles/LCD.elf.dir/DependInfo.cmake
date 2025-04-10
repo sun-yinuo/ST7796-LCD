94f7524b7c3ff6880fc54987d0e9b7e7d207000a
martin <2192519304@qq.com>
2025-04-10 21:14:09 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/LCD.elf.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/Administrator/Desktop/LCD/Core/Startup/startup_stm32l476rgtx.s" "C:/Users/Administrator/Desktop/LCD/cmake-build-debug/CMakeFiles/LCD.elf.dir/Core/Startup/startup_stm32l476rgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/Administrator/Desktop/LCD/Core/Inc"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Inc"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Administrator/Desktop/LCD/Core/Src/main.c" "CMakeFiles/LCD.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Core/Src/stm32l4xx_hal_msp.c" "CMakeFiles/LCD.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Core/Src/stm32l4xx_it.c" "CMakeFiles/LCD.elf.dir/Core/Src/stm32l4xx_it.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Core/Src/stm32l4xx_it.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Core/Src/syscalls.c" "CMakeFiles/LCD.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Core/Src/sysmem.c" "CMakeFiles/LCD.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Core/Src/system_stm32l4xx.c" "CMakeFiles/LCD.elf.dir/Core/Src/system_stm32l4xx.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Core/Src/system_stm32l4xx.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj.d"
+  "C:/Users/Administrator/Desktop/LCD/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d
@@ -0,0 +1,36 @@
+CMakeFiles/LCD.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj: \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Src\stm32l4xx_hal_rcc.c \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal.h \
+ C:\Users\Administrator\Desktop\LCD\Core\Inc/stm32l4xx_hal_conf.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_rcc.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_def.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\CMSIS\Device\ST\STM32L4xx\Include/stm32l4xx.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\CMSIS\Device\ST\STM32L4xx\Include/stm32l476xx.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\CMSIS\Include/core_cm4.h \
+ d:\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdint.h \
+ d:\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_default_types.h \
+ d:\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\features.h \
+ d:\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_newlib_version.h \
+ d:\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_intsup.h \
+ d:\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_stdint.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\CMSIS\Include/cmsis_version.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\CMSIS\Include/cmsis_compiler.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\CMSIS\Include/cmsis_gcc.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\CMSIS\Include/mpu_armv7.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\CMSIS\Device\ST\STM32L4xx\Include/system_stm32l4xx.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ d:\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_rcc_ex.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_gpio.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_gpio_ex.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_dma.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_cortex.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_exti.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_flash.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_flash_ex.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_flash_ramfunc.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_pwr.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_pwr_ex.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_spi.h \
+ C:\Users\Administrator\Desktop\LCD\Drivers\STM32L4xx_HAL_Driver\Inc/stm32l4xx_hal_spi_ex.h
